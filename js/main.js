d2ad6d2b03b32acc1df3bccdb3378d96ab0b2db3
Jack Walker <flybe@execs.com>
2019-01-17 13:36:53 +0000

    Adding Fuel and Fx graphs for #1
    + Correcting CNN Interview tweet > retweet
    + Images can now be clicked on to enlarge

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-(function(){
-    // Vertical Timeline - by CodyHouse.co
-	function VerticalTimeline( element ) {
+(function () {
+	// Vertical Timeline - by CodyHouse.co
+	function VerticalTimeline(element) {
 		this.element = element;
 		this.blocks = this.element.getElementsByClassName("js-cd-block");
 		this.images = this.element.getElementsByClassName("js-cd-img");
@@ -9,30 +9,30 @@
 		this.hideBlocks();
 	};
 
-	VerticalTimeline.prototype.hideBlocks = function() {
+	VerticalTimeline.prototype.hideBlocks = function () {
 		//hide timeline blocks which are outside the viewport
-		if ( !"classList" in document.documentElement ) {
+		if (!"classList" in document.documentElement) {
 			return;
 		}
 		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
-					self.images[i].classList.add("cd-is-hidden"); 
-					self.contents[i].classList.add("cd-is-hidden"); 
+		for (var i = 0; i < this.blocks.length; i++) {
+			(function (i) {
+				if (self.blocks[i].getBoundingClientRect().top > window.innerHeight * self.offset) {
+					self.images[i].classList.add("cd-is-hidden");
+					self.contents[i].classList.add("cd-is-hidden");
 				}
 			})(i);
 		}
 	};
 
-	VerticalTimeline.prototype.showBlocks = function() {
-		if ( ! "classList" in document.documentElement ) {
+	VerticalTimeline.prototype.showBlocks = function () {
+		if (!"classList" in document.documentElement) {
 			return;
 		}
 		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
+		for (var i = 0; i < this.blocks.length; i++) {
+			(function (i) {
+				if (self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight * self.offset) {
 					// add bounce-in animation
 					self.images[i].classList.add("cd-timeline__img--bounce-in");
 					self.contents[i].classList.add("cd-timeline__content--bounce-in");
@@ -46,26 +46,51 @@
 	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
 		verticalTimelinesArray = [],
 		scrolling = false;
-	if( verticalTimelines.length > 0 ) {
-		for( var i = 0; i < verticalTimelines.length; i++) {
-			(function(i){
+	if (verticalTimelines.length > 0) {
+		for (var i = 0; i < verticalTimelines.length; i++) {
+			(function (i) {
 				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
 			})(i);
 		}
 
 		//show timeline blocks on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
+		window.addEventListener("scroll", function (event) {
+			if (!scrolling) {
 				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
+				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250): window.requestAnimationFrame(checkTimelineScroll);
 			}
 		});
 	}
 
 	function checkTimelineScroll() {
-		verticalTimelinesArray.forEach(function(timeline){
+		verticalTimelinesArray.forEach(function (timeline) {
 			timeline.showBlocks();
 		});
 		scrolling = false;
 	};
+
+	document.querySelectorAll("img[data-activate]").forEach(e => {
+		e.addEventListener('click', function () {
+			activate(e.dataset.activate)
+		});
+	})
+
+	document.querySelector(".lightbox-close").addEventListener('click', close)
+
+	function activate(id) {
+		document.querySelector(".lightbox-target").classList.add("active");
+
+		document.querySelectorAll(".lightbox-target img").forEach(e => {
+			if (e.id === id) {
+				e.classList.add('vis');
+			} else {
+				e.classList.remove('vis');
+			}
+		});
+
+	}
+
+	function close() {
+		document.querySelector(".lightbox-target").classList.remove("active");
+	}
 })();
