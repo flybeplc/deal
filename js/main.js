bf98af89218b04a141769a72e7f59a3c9c82589c
Jack Walker <flybeplc@protonmail.com>
2019-01-21 18:44:39 +0000

    Update for filtering - Presentational changes only

--- js/main.js
+++ js/main.js
@@ -1,73 +1,5 @@
 (function () {
-	// Vertical Timeline - by CodyHouse.co
-	function VerticalTimeline(element) {
-		this.element = element;
-		this.blocks = this.element.getElementsByClassName("js-cd-block");
-		this.images = this.element.getElementsByClassName("js-cd-img");
-		this.contents = this.element.getElementsByClassName("js-cd-content");
-		this.offset = 0.8;
-		this.hideBlocks();
-	};
 
-	VerticalTimeline.prototype.hideBlocks = function () {
-		//hide timeline blocks which are outside the viewport
-		if (!"classList" in document.documentElement) {
-			return;
-		}
-		var self = this;
-		for (var i = 0; i < this.blocks.length; i++) {
-			(function (i) {
-				if (self.blocks[i].getBoundingClientRect().top > window.innerHeight * self.offset) {
-					self.images[i].classList.add("cd-is-hidden");
-					self.contents[i].classList.add("cd-is-hidden");
-				}
-			})(i);
-		}
-	};
-
-	VerticalTimeline.prototype.showBlocks = function () {
-		if (!"classList" in document.documentElement) {
-			return;
-		}
-		var self = this;
-		for (var i = 0; i < this.blocks.length; i++) {
-			(function (i) {
-				if (self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight * self.offset) {
-					// add bounce-in animation
-					self.images[i].classList.add("cd-timeline__img--bounce-in");
-					self.contents[i].classList.add("cd-timeline__content--bounce-in");
-					self.images[i].classList.remove("cd-is-hidden");
-					self.contents[i].classList.remove("cd-is-hidden");
-				}
-			})(i);
-		}
-	};
-
-	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
-		verticalTimelinesArray = [],
-		scrolling = false;
-	if (verticalTimelines.length > 0) {
-		for (var i = 0; i < verticalTimelines.length; i++) {
-			(function (i) {
-				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
-			})(i);
-		}
-
-		//show timeline blocks on scrolling
-		window.addEventListener("scroll", function (event) {
-			if (!scrolling) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250): window.requestAnimationFrame(checkTimelineScroll);
-			}
-		});
-	}
-
-	function checkTimelineScroll() {
-		verticalTimelinesArray.forEach(function (timeline) {
-			timeline.showBlocks();
-		});
-		scrolling = false;
-	};
 
 	document.querySelectorAll("img[data-activate]").forEach(e => {
 		e.addEventListener('click', function () {
@@ -87,12 +19,62 @@
 				e.classList.remove('vis');
 			}
 		});
+	}
+
+	document.querySelectorAll('.tl__img').forEach(e => {
+		e.addEventListener('click', function (e) {
+			var n = e.target.parentNode;
+			if (n) {
+				updateFilter([n], !n.dataset['filter']);
+			}
+		})
+	});
+
+	var ta = document.querySelectorAll('.announcement'),
+		tb = document.querySelectorAll('.background'),
+		tm = document.querySelectorAll('.market');
+
+	var fa = document.getElementById('toggle-announcement'),
+		fb = document.getElementById('toggle-background'),
+		fm = document.getElementById('toggle-market');
+
+
+	document.getElementById('count-announcement').innerText = ta.length;
+	document.getElementById('count-background').innerText = tb.length;
+	document.getElementById('count-market').innerText = tm.length;
+
+	document.getElementById('LatestPage').addEventListener('click', function () {
+		location.reload(true)
+	});
+
+	fa.addEventListener('change', function (e) {
+		updateFilter(ta, !e.target.checked);
+	});
+
+	fb.addEventListener('change', function (e) {
+		updateFilter(tb, !e.target.checked);
+	});
+
+	fm.addEventListener('change', function (e) {
+		updateFilter(tm, !e.target.checked);
+	});
+
+	function updateFilter(arr, set) {
+		if (arr && arr.forEach) {
+			arr.forEach(e => {
+				if (set) {
+					e.setAttribute('data-filter', 'on')
+				} else {
+					e.removeAttribute('data-filter')
+				}
+			});
+		}
 
 	}
 
-	document.getElementById('LatestPage').addEventListener('click', function () {
-		Location.relod(true)
-	});
+	updateFilter(ta, !fa.checked);
+	updateFilter(tb, !fb.checked);
+	updateFilter(tm, !fm.checked);
 
 	function close() {
 		document.querySelector(".lightbox-target").classList.remove("active");
